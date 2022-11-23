c234000b527691981cbbc95701fa6b1ff092b4ca
mkjeonstudy <mkjeonstudy@gmail.com>
2022-11-23 11:48:06 +0900

    게시물 리스트 구현

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -40,15 +40,19 @@ public class Main {
 				Article article = new Article(id, title, body);
 				articles.add(article);
 
-				System.out.printf("%d번 글이 생성 되었습니다.", id);
+				System.out.printf("%d번 글이 생성 되었습니다.\n", id);
 			}
 
 			else if (command.equals("article list")) {
 
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다.");
-				} else {
-					System.out.println("게시글이 있습니다.");
+					continue;
+				}
+				System.out.println("번호        /        제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d        /        %s\n", article.id, article.title);
 				}
 			}
 
@@ -61,6 +65,7 @@ public class Main {
 		System.out.println("==프로그램 종료==");
 		sc.close();
 	}
+
 }
 
 class Article {
